104f870df01647fae5003757839a3e9abce7de1c
takaya <takaya1912@gmail.com>
2021-07-03 22:44:07 +0900

    fix

--- server/src/java_test/ClassInheritance.java
+++ server/src/java_test/ClassInheritance.java
@@ -1,19 +1,19 @@
 public class ClassInheritance {
     public static void main(String[] args) {
         Cat cat = new Cat();
-        System.out.println(cat.power);
-        cat.run(50);
+        cat.run();
     }
 }
 
 class Animal {
     int power = 100;
+    void run() {
+        System.out.println(power);
+    }
 }
 class Cat extends Animal {
     int power = 200;
     void run(int power) {
-        System.out.println(power);
-        System.out.println(this.power);
-        System.out.println(super.power);
+        super.run();
     }
 }
